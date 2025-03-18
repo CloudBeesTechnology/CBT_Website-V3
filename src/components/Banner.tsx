9e250eb62416dc84a994d6c1228e0dfeaa01d2d7
hari1614 <hariharanofficial2812@gmail.com>
2025-03-18 12:31:07 +0530

    Updated.

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,3 +1,53 @@
+// import Image from "next/image";
+// import BubbleImg from "../assets/HomeSection/Banner/Images/Bubble.png";
+
+// interface BannerProps {
+//     homeLogo: string;
+//     homeBannerText: React.ReactNode;
+// }
+
+// export const Banner: React.FC<BannerProps> = ({ homeLogo, homeBannerText }) => {
+//     return (
+//         <div className="relative bg_image overflow-hidden">
+//             <div className="absolute bg-blend-overlay animate-move bottom-0">
+//                 <Image
+//                     src={BubbleImg}
+//                     alt="CBT Banner Image"
+//                     width={500}
+//                     height={100}
+//                     className="animate-spin-custom shadow-lg blur-[60px]"
+//                 />
+//             </div>
+//             {/* Home */}
+//             <div className="flex items-center justify-center min-h-screen text-white">
+//                 <div className="flex justify-between gap-88">
+//                     <div className="flex items-center justify-center">
+//                         {/* Logo Wrapper */}
+//                         <div className="relative">
+//                             <Image
+//                                 src={homeLogo}
+//                                 alt="Home Logo"
+//                                 width={150}
+//                                 height={50}
+//                                 className="animate-bounce-custom "
+//                             />
+//                             <div className="absolute bottom-0 left-0 right-0 h-[6px] bg-transparent shadow-[0_4px_10px_rgba(0,0,0,0.3)]"></div>
+//                         </div>
+//                     </div>
+
+//                     <div className="flex items-center justify-center">
+//                         <h2 className="text-3xl font-bold">{homeBannerText}</h2>
+//                     </div>
+//                 </div>
+//             </div>
+
+//             {/* Service */}
+//             <div>
+
+//             </div>
+//         </div>
+//     );
+// };
 import Image from "next/image";
 import BubbleImg from "../assets/HomeSection/Banner/Images/Bubble.png";
 
@@ -18,19 +68,23 @@ export const Banner: React.FC<BannerProps> = ({ homeLogo, homeBannerText }) => {
                     className="animate-spin-custom shadow-lg blur-[60px]"
                 />
             </div>
-             {/* Home */}
+            {/* Home */}
             <div className="flex items-center justify-center min-h-screen text-white">
                 <div className="flex justify-between gap-88">
-                    <div className="animate-bounce-custom">
-                        <Image
-                            src={homeLogo}
-                            alt="Home Logo"
-                            width={150}
-                            height={50}
-                         
-                        />
-                        <div className="animate-bounce-custom-shadow border"></div>
+                    <div className="flex items-center justify-center">
+                        {/* Logo Wrapper */}
+                        <div className="relative py-12">
+                            <Image
+                                src={homeLogo}
+                                alt="Home Logo"
+                                width={150}
+                                height={50}
+                                className="animate-bounce-custom "
+                            />
+                            <div className="animate-bounce-custom-shadow absolute bottom-0 left-0 right-0 h-[6px] bg-transparent shadow-[0_4px_10px_rgba(0,0,0,0.3)]"></div>
+                        </div>
                     </div>
+
                     <div className="flex items-center justify-center">
                         <h2 className="text-3xl font-bold">{homeBannerText}</h2>
                     </div>
