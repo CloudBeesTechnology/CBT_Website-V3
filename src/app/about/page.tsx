5df26682f2ceb6b7773b2edd2c47022367b0c738
Sriram281 <sriramc2810@gmail.com>
2025-03-20 16:19:29 +0530

    updated at 4:17pm

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,104 @@
-// src/app/about/page.tsx
-export default function about() {
-  return (
-    <div className="container mx-auto text-center mt-10">
-      <h1 className="text-4xl font-bold mb-4">About Us</h1>
-      <p className="text-lg">Learn more about our company, our values, and our mission.</p>
-    </div>
+ "use client"
+import Image from "next/image";
+import aboutUs from "../../assets/AboutSection/HeaderImages/aboutUs.png";
+import ourMission from "../../assets/AboutSection/HeaderImages/ourMission.png";
+import { Banner } from "@/components/Banner";
+import Frontiers from "./frontiers";
+import { employees } from "./employeesData";
+import { useEffect, useState } from "react";
+
+
+const About: React.FC = () => {
+  const [rotate, setRotate] = useState<boolean>(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setRotate(true);
+    }, 200); 
+
+    return () => clearTimeout(timer); 
+  }, []);
+  const aboutBannerText = (
+    <>
+      <div className="flex justify-center items-start space-x-10">
+        {/* Empowering */}
+        <div className="flex items-center space-y-1">
+          <span className="text-white text_size_1 ">
+            Unveiling CBT a Narrative in Progress
+          </span>
+        </div>
+      </div>
+    </>
   );
-}
+
+  return (
+    <>
+      <Banner aboutBannerText={aboutBannerText} rotate={rotate}/>
+
+      <div className="flex flex-col lg:flex-row justify-around items-center p-6 rounded-lg screen-size">
+        <div className="w-full lg:w-1/2 flex lg:justify-start sm:justify-center">
+          <Image
+            src={aboutUs}
+            alt="logo"
+            width={400}
+            height={450}
+            className="max-w-full object-cover"
+          />
+        </div>
+
+        <div className="w-full lg:w-1/2 flex flex-col justify-center items-center text-center ">
+          <h2 className="title mb-6 p-4">
+            <span className="text-yellow">About </span>
+            <span className="text-dark_gray">Us</span>
+          </h2>
+
+          <p className="text-para text-gray">
+            At CloudBees Tech, we don’t just deliver solutions—we create
+            pathways to your success. We connect your projects with innovative
+            solutions, turning your ideas into reality. With a foundation built
+            on trust, excellence, and reliability, our priority is your growth.
+            Our clients have seen firsthand the benefits of our collaborative
+            spirit and expertise. We know your industry inside out and stay
+            ahead of trends to keep you in the lead. Partner with us, and
+            experience the difference of a team dedicated to your success.
+          </p>
+        </div>
+      </div>
+
+      <div className="flex flex-col items-center  p-6 rounded-lg  screen-size">
+        <h2 className="title mb-4 text-center">
+          <span className="text-gray">Our </span>
+          <span className="text-yellow">Mission</span>
+        </h2>
+
+        <div className="flex flex-col lg:flex-row w-full justify-between items-center space-y-6 lg:space-y-0">
+          <div className="w-full lg:w-1/2 flex flex-col justify-center  lg:p-6 text-center space-y-4">
+            <h6 className="title_2 text-gray">
+              CloudBees Tech brings your ideas to life.
+            </h6>
+            <p className="text-para text-gray">
+              Our mission is to deliver top-notch software solutions that spark
+              innovation and drive success. We’re the bridge between your ideas
+              and their potential, turning challenges into triumphs with
+              creativity, trust, and a relentless commitment to excellence.
+              Let’s make your next breakthrough our shared success!
+            </p>
+          </div>
+
+          <div className="w-full lg:w-1/2 flex justify-center lg:justify-end">
+            <Image
+              src={ourMission}
+              alt="ourMission"
+              width={400}
+              height={450}
+              className="rounded-lg max-w-full"
+            />
+          </div>
+        </div>
+      </div>
+      <Frontiers employees={employees} />
+    </>
+  );
+};
+
+export default About;
--- src/app/home/Progress.jsx
+++ src/app/home/Progress.jsx
@@ -2,7 +2,7 @@ import { ProgressDetails } from "./ProgressDetails";
 export default function Progress () {
   return (
    
-      <section className="screen-size flex items-center justify-between flex-wrap gap-10 my-10  rounded-lg ">
+      <section className="screen-size flex items-center justify-between flex-wrap gap-10 my-10 rounded-lg ">
         {ProgressDetails.map((value,i)=>{
           return(
             <div key={i} className=" space-y-4 px-10 text-center border-r border-[#909090] nth-of-type-[4]:border-none ">
