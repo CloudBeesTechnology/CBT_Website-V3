aa55b538cb5d67e4108d6fc1bb5344d411a3ed4f
karthikeyan <karthikeyandeveloper0307@gmail.com>
2025-03-24 16:06:11 +0530

    kk

--- src/app/careers/Careers.jsx
+++ src/app/careers/Careers.jsx
@@ -11,36 +11,36 @@ export default function Careers() {
         <span className="text-primary">Job </span> Openings
       </h2>
 
-   <div className=" relative flex-col center ">
-   <Image
-              src={CareerImg}
-              alt="Career Image"
-              className="max-sm:w-full max-w-lg"
-            />
-   <div className=" absolute sm:bottom-20 space-y-">
-            <h2 className="carrer_text text-center">
-              Stay tuned for more updates{" "}<br/>
-              <span className="text-primary"> Coming your way soon </span>
-            </h2>
-            <Image
-              src={CareerArrow}
-              alt="Career Image"
-              className="mx-auto max-sm:w-[50px] mb-5"
-              width={94}
-              height={160}
-            />
-            <p>
-              <a
-                href="hari"
-                className="text-blue-500 underline carrer_link_text hover:underline center"
-              >
-                Connect with us
-              </a>
-            </p>
-          </div>
-   </div>
-          
-       
+      <div className=" relative flex-col center ">
+        <Image
+          src={CareerImg}
+          alt="Career Image"
+          className="max-sm:w-full max-w-lg"
+        />
+        <div className=" absolute sm:bottom-20 ">
+          <h2 className="sm:text-2xl text-xl text-center">
+            Stay tuned for more updates{" "}<br />
+            <span className="text-primary"> Coming your way soon </span>
+          </h2>
+          <Image
+            src={CareerArrow}
+            alt="Career Image"
+            className="mx-auto max-sm:w-[50px] mb-5"
+            width={94}
+            height={160}
+          />
+          <p>
+            <a
+              href="hari"
+              className="text-blue-500 text-lg sm:text-xl underline hover:underline center"
+            >
+              Connect with us
+            </a>
+          </p>
+        </div>
+      </div>
+
+
     </div>
   );
 }
